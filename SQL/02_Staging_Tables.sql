2b8ac47b4241bb6a2ef08b5f0605765b845d1100
nehasuva-engineer <nehasuva2611@gmail.com>
2026-09-25 19:59:37 +0530

    Update 02_Staging_Tables.sql

--- SQL/02_Staging_Tables.sql
+++ SQL/02_Staging_Tables.sql
@@ -1,1 +1,21 @@
+USE Airline;
+GO
 
+CREATE TABLE STG.STG_MEMBER
+(
+    Member_Name        VARCHAR(255),
+    Member_ID          VARCHAR(18),
+    Enrollment_Date    DATE,
+    Last_Flight_Date   DATE,
+    Tier_Code          VARCHAR(5),
+    Agent_Name         VARCHAR(255),
+    State              VARCHAR(5),
+    Country            VARCHAR(5),
+    Post_Code          INT,
+    DOB                DATE,
+    Active_Member      CHAR(1),
+    Age                INT,
+    Stale_Member       CHAR(1),
+    Load_Date          DATETIME
+);
+GO
